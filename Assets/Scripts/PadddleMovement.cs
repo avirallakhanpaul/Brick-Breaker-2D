b5d59183a10d8fe7aeab367264d7e30c546cd46b
avirallakhanpaul <avirallakhanpaul8@gmail.com>
2020-10-13 15:10:46 +0530

    Added Paddle and its Properties

---
+++ Assets/Scripts/PadddleMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadddleMovement : MonoBehaviour {
+    public float horizontalSpeed = 5.0f;
+    private float leftScreenEdge = -1.71f;
+    private float rightScreenEdge = 1.71f;
+    void Start() {
+
+    }
+    void Update() {
+
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontalAxis * horizontalSpeed * Time.deltaTime);
+
+        if(transform.position.x > 1.71) {
+            transform.position = new Vector2(rightScreenEdge, transform.position.y);
+        }
+
+        if(transform.position.x < -1.71) {
+            transform.position = new Vector2(leftScreenEdge, transform.position.y);
+        }
+    }
+}
