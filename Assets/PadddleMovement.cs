ab6ab0e36181460b28c7fe8fbfa2e79beb94e770
avirallakhanpaul <avirallakhanpaul8@gmail.com>
2020-10-13 13:46:47 +0530

    Initial Commit

---
+++ Assets/PadddleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadddleMovement : MonoBehaviour
+{
+
+    public horizontalSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        public horizontalAxis = Input.GetAxis("Horizontal");
+
+        transform.Translate.right = horizontalAxis * horizontalSpeed * Time.deltaTime;
+    }
+}
