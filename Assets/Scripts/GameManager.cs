af99a221d4396c6ecc687d79dd900d27ea364418
avirallakhanpaul <avirallakhanpaul8@gmail.com>
2020-10-14 11:53:52 +0530

    Added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public int score;
+    public int lives;
+    public Text scoreText;
+    public Text livesText;
+    void Start() {
+        setScore();
+        setLives();
+    }
+
+    void Update() {
+        
+    }
+
+    void setScore() {
+        scoreText.text = "Score: " + score;
+    }
+
+    void setLives() {
+        livesText.text = "Lives: " + lives;
+    }
+
+    public void updateLives(int change) {
+        lives += change;
+        setLives();
+    }
+
+    public void updateScore(int change) {
+        score += change;
+        setScore();
+    }
+}
