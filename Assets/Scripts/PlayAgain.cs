1af4c5ca7839e70aa8527984b461888b4367f14f
avirallakhanpaul <avirallakhanpaul8@gmail.com>
2020-10-15 12:37:44 +0530

    Added Play Again Button

---
+++ Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayAgain : MonoBehaviour
+{
+    public void loadLevel() {
+        SceneManager.LoadScene("Level01");
+    }
+}
